04b992bbe67bb1f8cd9de0663b8a33269a0d7d6a
aniliso <tkaptan41@gmail.com>
2017-08-16 21:06:48 +0300

    Version 2.5.2

---
+++ Events/Handlers/RegisterSitemapSidebar.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Modules\Sitemap\Events\Handlers;
+
+use Maatwebsite\Sidebar\Group;
+use Maatwebsite\Sidebar\Item;
+use Maatwebsite\Sidebar\Menu;
+use Modules\Core\Sidebar\AbstractAdminSidebar;
+
+class RegisterSitemapSidebar extends AbstractAdminSidebar
+{
+    /**
+     * @param Menu $menu
+     *
+     * @return Menu
+     */
+    public function extendWith(Menu $menu)
+    {
+        return $menu;
+    }
+}
