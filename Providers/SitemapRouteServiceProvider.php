ffce174145cb055fef8a5cb7383731fd95ff7fd4
sagsoz06 <designer@visualturk.net>
2018-12-31 01:59:57 +0300

    Fixed displaying one language alternates

--- Providers/SitemapRouteServiceProvider.php
+++ Providers/SitemapRouteServiceProvider.php
@@ -2,28 +2,32 @@
 
 use Illuminate\Routing\Router;
 use Modules\Core\Providers\RoutingServiceProvider as CoreRoutingServiceProvider;
+use File;
 
 abstract class SitemapRouteServiceProvider extends CoreRoutingServiceProvider
 {
     public function map(Router $router)
     {
-        $modules = $this->app['modules']->getOrdered('asc');
-        $defaultLocale = array_first(array_keys(\LaravelLocalization::getSupportedLocales()));
-        $router->group(['namespace' => $this->namespace], function (Router $router) use ($modules, $defaultLocale) {
+        $modules = app('modules');
+        $router->group(['namespace' => $this->namespace], function (Router $router) use ($modules) {
             require $this->getFrontendRoute();
-            foreach ($modules as $module) {
-                if(\Module::active($module->getName())) {
+            $defaultLocale = config('app.locale');
+            $prefix        = config('laravellocalization.hideDefaultLocaleInURL') === true ? '' : $defaultLocale;
+            foreach ($modules->getOrdered('asc') as $module) {
+                if($modules->active($module->getName())) {
                     $sitemapPath = $module->getPath().'/Http/Controllers/SitemapController.php';
-                    if(\File::exists($sitemapPath)) {
-                        $router->group(['prefix'=>$defaultLocale, 'middleware'=>['web']], function(Router $router) use ($module) {
+                    if(File::exists($sitemapPath)) {
+                        $router->group(['prefix'=>$prefix, 'middleware'=>['web']], function(Router $router) use ($module) {
                             $router->get(strtolower($module->getName()).'/sitemap.xml',[
-                                'uses' => '\Modules\\'.$module->getName().'\\Http\Controllers\SitemapController@index',
+                                'uses' => "\Modules\\{$module->getName()}\\Http\Controllers\SitemapController@index",
                                 'as' => strtolower($module->getName()).'.sitemap'
                             ]);
                         });
                     }
+
                 }
             }
+
         });
     }
 }
